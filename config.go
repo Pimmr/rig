9d65b3e7f42319d08eee8693119dffd7820e1e11
Guillaume de Sagazan <yazgazan@gmail.com>
2018-12-11 13:48:11 +0100

    A few ideas

--- config.go
+++ config.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"time"
 
 	"github.com/pkg/errors"
 )
@@ -42,13 +43,13 @@ func (f Flag) missingError() error {
 	return errors.Errorf("missing environment variable %s", f.Env)
 }
 
-func Parse(arguments []string, flags ...*Flag) error {
+func Parse(flags ...*Flag) error {
 	config := &Config{
 		FlagSet: flag.NewFlagSet(os.Args[0], flag.ExitOnError),
 		Flags:   flags,
 	}
 
-	return config.Parse(arguments)
+	return config.Parse(os.Args[1:])
 }
 
 type Config struct {
@@ -58,9 +59,15 @@ type Config struct {
 }
 
 func (c *Config) Parse(arguments []string) error {
-	c.FlagSet.Usage = func() { PrintUsage(c) }
+	// TODO(yazgazan): support https://golang.org/pkg/flag/#FlagSet.ErrorHandling
+	// TODO(yazgazan): support https://golang.org/pkg/flag/#FlagSet.Arg
+	// TODO(yazgazan): support https://golang.org/pkg/flag/#FlagSet.Args
+	c.FlagSet.Usage = c.Usage
 
 	for _, f := range c.Flags {
+		if f.Name == "" {
+			continue
+		}
 		c.FlagSet.Var(f, f.Name, f.Usage)
 	}
 
@@ -102,64 +109,42 @@ func (c *Config) Parse(arguments []string) error {
 		hasErrors = true
 	}
 	if hasErrors {
-		PrintUsage(c)
+		c.Usage()
 		os.Exit(2)
 	}
 
 	return nil
 }
 
-func PrintUsage(config *Config) {
-	_, _ = fmt.Fprintf(config.FlagSet.Output(), "Usage of %s:\n", os.Args[0])
-	for _, f := range config.Flags {
+func (c *Config) Usage() {
+	_, _ = fmt.Fprintf(c.FlagSet.Output(), "Usage of %s:\n", os.Args[0])
+	for _, f := range c.Flags {
 		if f.Name == "" || f.Env == "" {
 			continue
 		}
 
 		if f.Name != "" && f.Env != "" {
-			_, _ = fmt.Fprintf(config.FlagSet.Output(), "  -%s value, %s=value", f.Name, f.Env)
+			_, _ = fmt.Fprintf(c.FlagSet.Output(), "  -%s value, %s=value", f.Name, f.Env)
 		} else if f.Name != "" {
-			_, _ = fmt.Fprintf(config.FlagSet.Output(), "  -%s value", f.Name)
+			_, _ = fmt.Fprintf(c.FlagSet.Output(), "  -%s value", f.Name)
 		} else if f.Env != "" {
-			_, _ = fmt.Fprintf(config.FlagSet.Output(), "  %s=value", f.Env)
+			_, _ = fmt.Fprintf(c.FlagSet.Output(), "  %s=value", f.Env)
 		}
 		if f.TypeHint != "" {
-			_, _ = fmt.Fprintf(config.FlagSet.Output(), " (%s)", f.TypeHint)
+			_, _ = fmt.Fprintf(c.FlagSet.Output(), " (%s)", f.TypeHint)
 		}
 
-		_, _ = fmt.Fprint(config.FlagSet.Output(), "\n")
+		_, _ = fmt.Fprint(c.FlagSet.Output(), "\n")
 		defaultValue := f.Value.String()
 		if f.Usage != "" {
-			_, _ = fmt.Fprintf(config.FlagSet.Output(), "        %s", f.Usage)
-			if defaultValue != "" {
-				_, _ = fmt.Fprintf(config.FlagSet.Output(), " (default %q)", defaultValue)
+			_, _ = fmt.Fprintf(c.FlagSet.Output(), "        %s", f.Usage)
+			if defaultValue != "" && !f.Required {
+				_, _ = fmt.Fprintf(c.FlagSet.Output(), " (default %q)", defaultValue)
 			}
-			_, _ = fmt.Fprint(config.FlagSet.Output(), "\n")
-		} else if defaultValue != "" {
-			_, _ = fmt.Fprintf(config.FlagSet.Output(), "        (default %q)", defaultValue)
+			_, _ = fmt.Fprint(c.FlagSet.Output(), "\n")
+		} else if defaultValue != "" && !f.Required {
+			_, _ = fmt.Fprintf(c.FlagSet.Output(), "        (default %q)\n", defaultValue)
 		}
-	}
-}
-
-type stringValue string
-
-func (s stringValue) String() string {
-	return string(s)
-}
-
-func (s *stringValue) Set(v string) error {
-	*s = stringValue(v)
-
-	return nil
-}
-
-func String(v *string, flag, env, usage string) *Flag {
-	return &Flag{
-		Value:    (*stringValue)(v),
-		Name:     flag,
-		Env:      env,
-		Usage:    usage,
-		TypeHint: "string",
 	}
 }
 
@@ -182,20 +167,123 @@ func Required(f *Flag) *Flag {
 	}
 }
 
+func HintType(f *Flag, typeHint string) *Flag {
+	return &Flag{
+		Value:    f.Value,
+		Name:     f.Name,
+		Env:      f.Env,
+		Usage:    f.Usage,
+		Required: f.Required,
+		TypeHint: typeHint,
+	}
+}
+
+func Var(v flag.Value, flag, env, usage string) *Flag {
+	return &Flag{
+		Value: v,
+		Name:  flag,
+		Env:   env,
+		Usage: usage,
+	}
+}
+
+type stringValue string
+
+func (s stringValue) String() string {
+	return string(s)
+}
+
+func (s *stringValue) Set(val string) error {
+	*s = stringValue(val)
+	return nil
+}
+
+func String(v *string, flag, env, usage string) *Flag {
+	return &Flag{
+		Value:    (*stringValue)(v),
+		Name:     flag,
+		Env:      env,
+		Usage:    usage,
+		TypeHint: "string",
+	}
+}
+
+type boolValue bool
+
+func (b boolValue) String() string {
+	return strconv.FormatBool(bool(b))
+}
+
+func (b *boolValue) Set(s string) error {
+	v, err := strconv.ParseBool(s)
+	*b = boolValue(v)
+	return err
+}
+
+func Bool(v *bool, flag, env, usage string) *Flag {
+	return &Flag{
+		Value:    (*boolValue)(v),
+		Name:     flag,
+		Env:      env,
+		Usage:    usage,
+		TypeHint: "boolean",
+	}
+}
+
+type durationValue time.Duration
+
+func (d durationValue) String() string {
+	return time.Duration(d).String()
+}
+
+func (d *durationValue) Set(s string) error {
+	v, err := time.ParseDuration(s)
+	*d = durationValue(v)
+	return err
+}
+
+func Duration(v *time.Duration, flag, env, usage string) *Flag {
+	return &Flag{
+		Value:    (*durationValue)(v),
+		Name:     flag,
+		Env:      env,
+		Usage:    usage,
+		TypeHint: "duration",
+	}
+}
+
+type float64Value float64
+
+func (f float64Value) String() string {
+	return strconv.FormatFloat(float64(f), 'g', -1, 64)
+}
+
+func (f *float64Value) Set(s string) error {
+	v, err := strconv.ParseFloat(s, 64)
+	*f = float64Value(v)
+	return err
+}
+
+func Float64(v *float64, flag, env, usage string) *Flag {
+	return &Flag{
+		Value:    (*float64Value)(v),
+		Name:     flag,
+		Env:      env,
+		Usage:    usage,
+		TypeHint: "float",
+	}
+}
+
 type intValue int
 
 func (i intValue) String() string {
 	return strconv.Itoa(int(i))
 }
 
-func (i *intValue) Set(v string) error {
-	parsed, err := strconv.Atoi(v)
-	if err != nil {
-		return err
-	}
-
-	*i = intValue(parsed)
-	return nil
+func (i *intValue) Set(s string) error {
+	v, err := strconv.ParseInt(s, 0, strconv.IntSize)
+	*i = intValue(v)
+	return err
 }
 
 func Int(v *int, flag, env, usage string) *Flag {
@@ -205,5 +293,96 @@ func Int(v *int, flag, env, usage string) *Flag {
 		Env:      env,
 		Usage:    usage,
 		TypeHint: "integer",
+	}
+}
+
+type int64Value int64
+
+func (i int64Value) String() string {
+	return strconv.Itoa(int(i))
+}
+
+func (i *int64Value) Set(s string) error {
+	v, err := strconv.ParseInt(s, 0, 64)
+	*i = int64Value(v)
+	return err
+}
+
+func Int64(v *int64, flag, env, usage string) *Flag {
+	return &Flag{
+		Value:    (*int64Value)(v),
+		Name:     flag,
+		Env:      env,
+		Usage:    usage,
+		TypeHint: "64 bit integer",
+	}
+}
+
+type uintValue uint
+
+func (i uintValue) String() string {
+	return strconv.FormatUint(uint64(i), 10)
+}
+
+func (i *uintValue) Set(s string) error {
+	v, err := strconv.ParseUint(s, 0, strconv.IntSize)
+	*i = uintValue(v)
+	return err
+}
+
+func Uint(v *uint, flag, env, usage string) *Flag {
+	return &Flag{
+		Value:    (*uintValue)(v),
+		Name:     flag,
+		Env:      env,
+		Usage:    usage,
+		TypeHint: "unsigned integer",
+	}
+}
+
+type uint64Value uint64
+
+func (i uint64Value) String() string {
+	return strconv.FormatUint(uint64(i), 10)
+}
+
+func (i *uint64Value) Set(s string) error {
+	v, err := strconv.ParseUint(s, 0, 64)
+	*i = uint64Value(v)
+	return err
+}
+
+func Uint64(v *uint64, flag, env, usage string) *Flag {
+	return &Flag{
+		Value:    (*uint64Value)(v),
+		Name:     flag,
+		Env:      env,
+		Usage:    usage,
+		TypeHint: "unsigned 64 bit integer",
+	}
+}
+
+// V is sugar for the default flag functions. It will panic if the type is not supported.
+// Supported types are: *string, *bool, *time.Duration, *float64, *int, *int64, *uint, *uint64
+func V(v interface{}, flag, env, usage string) *Flag {
+	switch t := v.(type) {
+	default:
+		panic(errors.Errorf("unsupported type %T. Supported types: *string, *bool, *time.Duration, *float64, *int, *int64, *uint, *uint64", v))
+	case *string:
+		return String(t, flag, env, usage)
+	case *bool:
+		return Bool(t, flag, env, usage)
+	case *time.Duration:
+		return Duration(t, flag, env, usage)
+	case *float64:
+		return Float64(t, flag, env, usage)
+	case *int:
+		return Int(t, flag, env, usage)
+	case *int64:
+		return Int64(t, flag, env, usage)
+	case *uint:
+		return Uint(t, flag, env, usage)
+	case *uint64:
+		return Uint64(t, flag, env, usage)
 	}
 }
--- examples/simple/main.go
+++ examples/simple/main.go
@@ -7,16 +7,50 @@ import (
 	"github.com/Pimmr/config"
 )
 
+type countTheDotsValue uint
+
+func (d countTheDotsValue) String() string {
+	if d == 0 {
+		return "<none>"
+	}
+
+	s := ""
+	for i := 0; i < int(d); i++ {
+		s += "."
+	}
+
+	return s
+}
+
+func (d *countTheDotsValue) Set(s string) error {
+	for _, c := range s {
+		if c != '.' {
+			return fmt.Errorf("%q is not a dot", c)
+		}
+	}
+
+	*d = countTheDotsValue(len(s))
+	return nil
+}
+
+func CountTheDots(v *uint, name, env, usage string) *config.Flag {
+	return config.HintType(
+		config.Var((*countTheDotsValue)(v), name, env, usage),
+		"dotdotdot",
+	)
+}
+
 func main() {
 	var (
 		flagA int
-		flagB = "foo"
+		flagB      = "foo"
+		flagC uint = 7
 	)
 
 	err := config.Parse(
-		os.Args[1:],
-		config.Required(config.Int(&flagA, "flag-a", "FLAG_A", "flag A")),
-		config.String(&flagB, "flag-b", "FLAG_B", "flag B"),
+		config.Required(config.V(&flagA, "flag-a", "FLAG_A", "flag A")),
+		config.V(&flagB, "flag-b", "FLAG_B", ""),
+		CountTheDots(&flagC, "flag-c", "FLAG_C", "flag C"),
 	)
 	if err != nil {
 		fmt.Printf("Error: %s\n", err)
@@ -25,4 +59,5 @@ func main() {
 
 	fmt.Printf("flagA: %d\n", flagA)
 	fmt.Printf("flagB: %q\n", flagB)
+	fmt.Printf("flagC: %d\n", flagC)
 }
