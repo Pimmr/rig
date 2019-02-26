90fe9b21448c51e2429aa97b263a6872e8fea487
Guillaume de Sagazan <yazgazan@gmail.com>
2019-02-26 13:48:56 +0100

    Adding tests for Required

--- config_test.go
+++ config_test.go
@@ -102,6 +102,40 @@ func TestConfigUsage(t *testing.T) {
 	}
 }
 
+func TestRequired(t *testing.T) {
+	var s string
+
+	f := String(&s, "string-flag", "STRING_ENV", "testing Required on String")
+	r := Required(f)
+
+	if f.Required {
+		t.Errorf("String(...).Required = true, expected false")
+	}
+	if !r.Required {
+		t.Errorf("Required(String(...)).Required = false, expected true")
+	}
+
+	expectedSuffix := ", required"
+	if !strings.HasSuffix(r.TypeHint, expectedSuffix) {
+		t.Errorf("Required(String(...)).TypeHint = %q, expected %q suffix", r.TypeHint, expectedSuffix)
+	}
+
+	f = Var(new(stringValue), "var-flag", "VAR_ENV", "testing Required on Var")
+	r = Required(f)
+
+	if f.Required {
+		t.Errorf("Var(...).Required = true, expected false")
+	}
+	if !r.Required {
+		t.Errorf("Required(Var(...)).Required = false, expected true")
+	}
+
+	expected := "required"
+	if r.TypeHint != expected {
+		t.Errorf("Required(Var(...)).TypeHint = %q, expected %q", r.TypeHint, expected)
+	}
+}
+
 func TestRepeatable(t *testing.T) {
 	t.Run("int", func(t *testing.T) {
 		var ii []int
