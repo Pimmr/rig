6fc77f88a95059edd6af77c3c9e8dd38a7b0bf85
Guillaume de Sagazan <yazgazan@gmail.com>
2019-02-26 15:04:55 +0100

    Moving Flag to its own file

--- config.go
+++ config.go
@@ -11,53 +11,6 @@ import (
 	"github.com/pkg/errors"
 )
 
-type Flag struct {
-	flag.Value
-	Name     string
-	Env      string
-	Usage    string
-	Required bool
-	TypeHint string
-
-	set          bool
-	defaultValue string
-}
-
-type isBoolFlagger interface {
-	IsBoolFlag() bool
-}
-
-func (f *Flag) Set(v string) error {
-	err := f.Value.Set(v)
-	if err != nil {
-		return err
-	}
-
-	f.set = true
-	return nil
-}
-
-func (f *Flag) IsBoolFlag() bool {
-	if boolFlagger, ok := f.Value.(isBoolFlagger); ok {
-		return boolFlagger.IsBoolFlag()
-	}
-
-	return false
-}
-
-func (f Flag) missingError() error {
-	switch {
-	default:
-		return errors.New("configuration variable doesn't have a flag or environment variable specified")
-	case f.Name != "" && f.Env != "":
-		return errors.Errorf("missing command line flag -%s or environment variable %s", f.Name, f.Env)
-	case f.Name != "":
-		return errors.Errorf("missing command line flag -%s", f.Name)
-	case f.Env != "":
-		return errors.Errorf("missing environment variable %s", f.Env)
-	}
-}
-
 func Parse(flags ...*Flag) error {
 	config := &Config{
 		FlagSet: flag.NewFlagSet(os.Args[0], flag.ExitOnError),
--- config_test.go
+++ config_test.go
@@ -16,43 +16,6 @@ import (
 	"github.com/pkg/errors"
 )
 
-func TestFlagMissingError(t *testing.T) {
-	for _, test := range []struct {
-		flag               Flag
-		errorShouldContain []string
-	}{
-		{
-			flag:               Flag{Name: "", Env: ""},
-			errorShouldContain: []string{},
-		},
-		{
-			flag:               Flag{Name: "foo", Env: ""},
-			errorShouldContain: []string{"-foo"},
-		},
-		{
-			flag:               Flag{Name: "", Env: "BAR"},
-			errorShouldContain: []string{"BAR"},
-		},
-		{
-			flag:               Flag{Name: "foo", Env: "BAR"},
-			errorShouldContain: []string{"-foo", "BAR"},
-		},
-	} {
-		err := test.flag.missingError()
-		if err == nil {
-			t.Errorf("Flag(%+v).missingError(): expected error, got nil", test.flag)
-			continue
-		}
-
-		errStr := err.Error()
-		for _, s := range test.errorShouldContain {
-			if !strings.Contains(errStr, s) {
-				t.Errorf("Flag(%+v).missingError() = %q: expected to find %q in error string.", test.flag, errStr, s)
-			}
-		}
-	}
-}
-
 func TestConfigSetDefaultValues(t *testing.T) {
 	var (
 		s1 = "foo"
