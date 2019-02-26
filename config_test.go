d4a3bf5a6579e5e5c6cde29ec8b18265b2cc3837
Guillaume de Sagazan <yazgazan@gmail.com>
2019-02-26 14:58:08 +0100

    Adding tests for Config.handleError

--- config_test.go
+++ config_test.go
@@ -3,6 +3,9 @@ package config
 import (
 	"bytes"
 	"flag"
+	"io/ioutil"
+	"os"
+	"os/exec"
 	"reflect"
 	"regexp"
 	"strings"
@@ -129,6 +132,93 @@ func TestConfigArgArgs(t *testing.T) {
 	if !reflect.DeepEqual(c.Args(), expectedArgs) {
 		t.Errorf("Config.Args() = %q, expected %q", c.Args(), expectedArgs)
 	}
+}
+
+const testHandleErrorExitOnErrorEnv = "TEST_HANDLE_ERROR_CRASHER"
+
+var errTest = errors.New("test error")
+
+func TestHandleErrorExitOnError(t *testing.T) {
+	if os.Getenv(testHandleErrorExitOnErrorEnv) != "1" {
+		t.SkipNow()
+	}
+	c := &Config{
+		FlagSet: flag.NewFlagSet("flagset", flag.ExitOnError),
+	}
+
+	_ = c.handleError(errTest)
+	t.Logf("should have os.Exit, this code shouldn't have been reached")
+}
+
+func TestHandleError(t *testing.T) {
+	t.Run("flag.ExitOnError", func(t *testing.T) {
+		buf := &bytes.Buffer{}
+		cmd := exec.Command(os.Args[0], "-test.run=TestHandleErrorExitOnError")
+		cmd.Env = append(os.Environ(), testHandleErrorExitOnErrorEnv+"=1")
+		cmd.Stdout = ioutil.Discard
+		cmd.Stderr = buf
+
+		err := cmd.Run()
+		if e, ok := err.(*exec.ExitError); !ok || e.Success() {
+			t.Errorf("expected proccess to exit with error, got Success instead")
+		}
+
+		firstLine, err := buf.ReadString('\n')
+		if err != nil {
+			t.Errorf("unexpected error reading output's first line: %s", err)
+			t.FailNow()
+		}
+		if firstLine != errTest.Error()+"\n" {
+			t.Errorf("expected output buffer to start with %q, got %q instead", errTest, firstLine)
+		}
+	})
+
+	t.Run("flag.PanicOnError", func(t *testing.T) {
+		c := &Config{
+			FlagSet: flag.NewFlagSet("flagset", flag.PanicOnError),
+		}
+		buf := &bytes.Buffer{}
+		c.FlagSet.SetOutput(buf)
+		defer func() {
+			err := recover()
+			if err != errTest {
+				t.Errorf("expected panic to be %v, got %v instead", errTest, err)
+			}
+			firstLine, err := buf.ReadString('\n')
+			if err != nil {
+				t.Errorf("unexpected error reading output's first line: %s", err)
+				t.FailNow()
+			}
+			if firstLine != errTest.Error()+"\n" {
+				t.Errorf("expected output buffer to start with %q, got %q instead", errTest, firstLine)
+			}
+		}()
+
+		_ = c.handleError(errTest)
+		t.Errorf("should have panicked, this code shouldn't have been reached")
+	})
+
+	t.Run("flag.ContinueOnError", func(t *testing.T) {
+		c := &Config{
+			FlagSet: flag.NewFlagSet("flagset", flag.ContinueOnError),
+		}
+		buf := &bytes.Buffer{}
+		c.FlagSet.SetOutput(buf)
+
+		err := c.handleError(errTest)
+		if err != errTest {
+			t.Errorf("c.handleError(errTest) = %v, expected %v", err, errTest)
+		}
+
+		firstLine, err := buf.ReadString('\n')
+		if err != nil {
+			t.Errorf("unexpected error reading output's first line: %s", err)
+			t.FailNow()
+		}
+		if firstLine != errTest.Error()+"\n" {
+			t.Errorf("expected output buffer to start with %q, got %q instead", errTest, firstLine)
+		}
+	})
 }
 
 func TestConfigUsage(t *testing.T) {
